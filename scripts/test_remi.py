84ebae617083255a08ea0ba6c9e08f8c162cfa36
Remi Momprive <remi.momprive@gmail.com>
2018-12-20 11:54:53 +0100

    Test transformation coordonnees

--- scripts/test_remi.py
+++ scripts/test_remi.py
@@ -14,36 +14,37 @@ from __future__ import print_function, division
 
 import astropy.units as u
 from astropy.coordinates import SkyCoord
+from sunpy.coordinates import frames
 
 import sunpy.map
 import sunpy.data.sample
 import matplotlib.pyplot as plt
+from sunpy.net.helioviewer import HelioviewerClient
+from sunpy.map import Map
 
-##############################################################################
-# Sunpy sample data contains a number of suitable maps, where the
-# sunpy.data.sample.NAME returns the location of the given FITS file.
+swap_map = Map(sunpy.data.sample.SWAP_LEVEL1_IMAGE)
+print(swap_map.coordinate_frame)
 
-swap_map = sunpy.map.Map(sunpy.data.sample.SWAP_LEVEL1_IMAGE)
+hv = HelioviewerClient()
+filepath = hv.download_jp2('2012/07/05 00:30:00', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
+hmi = Map(filepath)
+#print(list(hmi.coordinate_frame.observer))
 
-##############################################################################
-# This has resolution and ranges of:
+#sc = SkyCoord(0*u.deg, 0*u.deg, 5*u.km, obstime="2012/07/05 00:30:00",frame="helioprojective", observer=SkyCoord(HeliographicStonyhurst(-10*u.deg, 2*u.deg)))
+#print(sc)
+#print(swap_map.data)
 
-print(swap_map.dimensions)
-print(swap_map.data)
+#print(swap_map.meta)
 
-##############################################################################
-# To find out more specifics about this map and the instrument used, check it's
-# metatdata:
+sc = SkyCoord(208.5*u.deg, -17.35*u.deg, 1*u.km,frame="heliographic_carrington", obstime="2012/07/05 00:30:00")
+sc = sc.transform_to(frames.Helioprojective)
 
-print(swap_map.meta)
+sc2 = SkyCoord(208.5*u.deg, -17.35*u.deg, 2*u.km,frame="heliographic_carrington", obstime="2012/07/05 00:30:00")
+sc2 = sc.transform_to(frames.Helioprojective)
 
-##############################################################################
-# To crop the data you create a submap, specifying the top right and bottom
-# left as SkyCoord objects.
+print(sc)
 
-top_right = SkyCoord(0*u.arcsec, -200 * u.arcsec, frame=swap_map.coordinate_frame)
-bottom_left = SkyCoord(-900 * u.arcsec, -900 * u.arcsec, frame=swap_map.coordinate_frame)
-swap_submap = swap_map.submap(bottom_left, top_right)
-swap_submap.peek(draw_limb=True, draw_grid=True)
+hmi = hmi.submap(sc, sc2)
+hmi.peek(draw_limb=True, draw_grid=True)
 plt.tight_layout(pad=1.00)
 plt.show()
