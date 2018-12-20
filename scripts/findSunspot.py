1b4ec1572459742a563e9979d7016b6039b53f73
Maxime Guilbault <maxrafalinoe@orange.fr>
2018-12-20 11:53:11 +0100

    sunspot

--- scripts/findSunspot.py
+++ scripts/findSunspot.py
@@ -6,34 +6,18 @@ import matplotlib.pyplot as plt
 from astropy.units import Quantity
 from sunpy.map import Map
 
-
 import sunpy.map
 import sunpy.data.sample
-import sunpy.map.mapbase
 
 hv = HelioviewerClient()
-filepath = hv.download_jp2('2012/07/05 00:30:00', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
+filepath = hv.download_jp2('2016/07/19 01:59:09', observatory='SDO', instrument='HMI', detector='HMI', measurement='continuum')
 hmi = Map(filepath)
 
-#            corners = u.Quantity([bottom_left, bottom_right, top_left, top_right])
-#        coord = SkyCoord(corners, frame=self.coordinate_frame)
-#    pixel_corners = self.world_to_pixel(coord)
-#a =SkyCoord(200*u.arcsec,500*u.arcsec,frame=hmi.coordinate_frame)
-#b =SkyCoord(-400*u.arcsec,200*u.arcsec,frame=hmi.coordinate_frame)
+#sc = SkyCoord(208.5*u.deg, -17.37*u.deg, frame="heliographic_carrington", obstime="2012/07/05 00:30:00")
+#sc = SkyCoord(-17.7*u.deg, 208.5*u.deg, obstime="2010/01/01T00:00:00", frame="helioprojective")
+#print(sc)
 #hmi = hmi.submap([2400,1400]*u.pixel,[3000,1750]*u.pixel)
-#print(a)
-#bottom_left = u.Quantity(a).to(u.pixel) #hmi.world_to_pixel(a,1)
-#top_right = u.Quantity(b).to(u.pixel)#hmi.world_to_pixel(b,1)
-#print(bottom_left)
-#hmi = hmi.submap(bottom_left,top_right)
-#top_right = hmi.wcs_world2pix(-1000*u.arcsec, -1000 * u.arcsec, frame=hmi.coordinate_frame)
-#bottom_left = SkyCoord(1000 * u.arcsec,  1000* u.arcsec, frame=hmi.coordinate_frame)
-coordinate = SkyCoord(200*u.arcsec,500*u.arcsec, frame=hmi.coordinate_frame)
-native_frame = coordinate.transform_to(hmi.coordinate_frame)
-lon, lat = u.Quantity(self._get_lon_lat(native_frame)).to(u.deg)
-x, y = hmi.wcs.wcs_world2pix(lon, lat, origin)
-
-#hmi = hmi.submap(bottom_left,top_right)
+#hmi = hmi.submap(sc)
 
 fig = plt.figure()
 # Provide the Map as a projection, which creates a WCSAxes object
